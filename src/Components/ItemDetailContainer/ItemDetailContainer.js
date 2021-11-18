2069c3a4d8186107af6ccc9396a972d8a97befc6
gballester <gballester@close-upinternational.com>
2021-11-18 19:49:40 -0300

    entrega Firebase I

--- src/Components/ItemDetailContainer/ItemDetailContainer.js
+++ src/Components/ItemDetailContainer/ItemDetailContainer.js
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from "react";
 import db from '../../firebase.config';
 import ItemDetail from "../ItemDetail/ItemDetail"
 import { useParams } from "react-router-dom"
-import Alert from '@mui/material/Alert';
-import ButtonGroup from '@mui/material/ButtonGroup'
 import Button from '@mui/material/Button'
 import { Link } from 'react-router-dom'
 
@@ -13,11 +11,6 @@ const ItemDetailContainer = () => {
     const { id } = useParams();
     const [item, setItem] = useState([])
 
-    const buttons = [
-        <Link to="/">
-            <Button key="goHome" variant="outlined" >Return to Home</Button>
-        </Link>
-    ]
 
     useEffect(() => {
         const prods = db.collection('Items').doc(id);
@@ -28,18 +21,12 @@ const ItemDetailContainer = () => {
                 setItem(newArry)
             }
         });
-    }, [id]);
+    },[id]);
 
     
     if (item.length === 0) {
         return (
-            // <p>loadding</p>
-            <div>
-                <Alert severity="warning">The requested document was not found in the database!!! Press the following button to see all the products on the main page. Sorry for the inconvenience.</Alert>
-                <ButtonGroup color="secondary" aria-label="medium secondary button group">
-                    {buttons}
-                </ButtonGroup>
-            </div>
+            <p>Loading product...</p>
         )
     } else {
         return (
